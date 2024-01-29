6b4c6de5309261e7d44b89953eb84def7bd817ed
recallone4934 <158097032+recallone4934@users.noreply.github.com>
2024-01-30 05:12:25 +0600

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,22 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 from threading import Thread
-import random
 
-app = Flask('')
-
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return 'Discord id : shellmonbhai    Server link : https://discord.gg/aGHMUec8Pk'
-
+def index():
+    return "Alive"
 
 def run():
-  app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+  app.run(host='0.0.0.0',port=8080)
 
-
-def keep_alive():
-  '''
-  Creates and starts new thread that runs the function run.
-  '''
-  t = Thread(target=run)
-  t.start()
+def keep_alive():  
+    t = Thread(target=run)
+    t.start()
